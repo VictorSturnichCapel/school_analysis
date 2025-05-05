511b8d17428eb72d86d98155cf6bc2b2d2d5590b
Victor.Capel <victor.capel@etapa.com.br>
2025-05-05 20:43:17 -0300

    upload files

--- sql.sql
+++ sql.sql
@@ -12,3 +12,12 @@ select w1.id as 'ID' from Weather as w left join Weather as w1 on w.recordDate =
 select t.request_at as 'Day', ROUND( SUM(CASE when t.status like 'cancelled%' then 1 else 0 END )/ COUNT(t.id) ,2) as 'Cancellation Rate' from Trips as t LEFT join Users as u 
 on t.client_id = u.users_id  LEFT join Users as u1 on t.driver_id = u1.users_id 
 where u.banned = 'No' and u1.banned = 'No' AND u.banned IS NOT NULL AND u1.banned IS NOT NULL and request_at between "2013-10-01" and "2013-10-03" group by t.request_at having COUNT(t.id) >= 1 
+
+select
+    student_id,
+    score,
+    percent_rank() over (order by score DESC) *100 as 'percent_rank'
+from 
+    students
+where
+    created_at >= CURRENT_DATE - INTERVAL '30 days'
