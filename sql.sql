d3cb277deb6cc5530100e9accecc1ae0a9c77c87
Victor.Capel <victor.capel@etapa.com.br>
2025-05-06 10:59:52 -0300

    atualizacao

--- sql.sql
+++ sql.sql
@@ -27,3 +27,35 @@ select w1.id as 'ID' from Weather as w left join Weather as w1 on w.recordDate =
 
 SELECT corr(coluna_x, coluna_y) AS correlacao
 FROM sua_tabela;
+
+select student_id, subject, min_scr as first_score, max_scr as latest_score  from (
+select student_id, subject, score, exam_date,
+FIRST_VALUE(score) OVER(PARTITION BY student_id, subject ORDER BY exam_date asc) as min_scr,
+LAST_VALUE(score) OVER(PARTITION BY student_id, subject ORDER BY exam_date asc) as max_scr,
+ROW_NUMBER() OVER(PARTITION BY student_id, subject ORDER BY exam_date desc) as rn
+from Scores) as q
+where rn = 1 and min_scr < max_scr
+
+SELECT 
+    (SUM((x - avg_x) * (y - avg_y)) /
+    SQRT(SUM(POW(x - avg_x, 2)) * SUM(POW(y - avg_y, 2)))) AS correlation
+FROM (
+    SELECT 
+        x_column AS x,
+        y_column AS y,
+        (SELECT AVG(x_column) FROM your_table) AS avg_x,
+        (SELECT AVG(y_column) FROM your_table) AS avg_y
+    FROM your_table
+) AS subquery;
+
+SELECT 
+    (SUM((x - avg_x) * (y - avg_y)) /
+    SQRT(SUM(POW(x - avg_x, 2)) * SUM(POW(y - avg_y, 2)))) AS correlation
+FROM (
+    SELECT 
+        student_id  AS x,
+        score  AS y,
+        (SELECT AVG(student_id) FROM Scores) AS avg_x,
+        (SELECT AVG(score ) FROM Scores) AS avg_y
+    FROM Scores 
+) AS subquery;
